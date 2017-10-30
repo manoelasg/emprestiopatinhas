0dadba655466ce2cbbc9db8b8584de5917332e0e
Alex Monteiro <alexcda@gmail.com>
2017-10-30 20:45:13 -0200

    Alteracoes no galera.js

--- js/controllers/galera.js
+++ js/controllers/galera.js
@@ -1,5 +1,14 @@
 angular.module('emprestiopatinhas').controller('galeraController', galeraController);
 
-function galeraController($scope,$state) {
+function galeraController($scope,$state,$http) {
+  if (1 == 1){
 
+
+  $http.get('http://localhost:3000/consulta/todos',{Authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OWNlZDY4ODc5YmQ1MzFjYTQxOTcyZDQiLCJpYXQiOjE1MDk0MDI1Njd9.X-dTtima6pbIVBDT3j57EGDi66T8jl9nK1VllBfKMyo"}).then(function(respostaSucesso){
+      console.log(`respostaSucesso: ${angular.toJson(respostaSucesso, 1)}`);
+      // $state.go("main");
+  }, function(respostaErro){
+      console.log(respostaErro);
+  });
+  }
 };
